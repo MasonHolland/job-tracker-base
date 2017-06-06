3b3f403a343d431863569cd66451c4ccefa40f46
MasonHolland <masontholland@gmail.com>
2017-06-06 17:09:39 -0600

    feature tests written

--- spec/features/companies/user_sees_one_company_spec.rb
+++ spec/features/companies/user_sees_one_company_spec.rb
@@ -11,4 +11,39 @@ describe "User sees one company" do
     expect(page).to have_content("ESPN")
     expect(page).to have_content("Developer")
   end
+
+  # As a user,
+  # When I visit a specific company page,
+  # I see the name of each of the office locations associated with that company.
+  scenario "the locations are present" do
+    company = Company.create!(name: "Dutch Boys Toys")
+    location_1 = Location.create(city: "Denver", company_id: company.id)
+    location_2 = Location.create(city: "Perth", company_id: company.id)
+
+    visit company_path(company)
+
+    expect(page).to have_content("Locations:")
+    expect(page).to have_content("Denver")
+    expect(page).to have_content("Perth")
+  end
+
+  # As a user,
+  # When I visit a specific company page,
+  # I also see a count of how many officies that company has in each specific location listed.
+
+  scenario "the locations have a count for the amount of offices" do
+    company = Company.create!(name: "Dutch Boys Toys")
+    location_1 = Location.create(city: "Denver", branch: "CBD", company_id: company.id)
+    location_2 = Location.create(city: "Denver", branch: "The Highlands", company_id: company.id)
+    location_3 = Location.create(city: "Perth", branch: "CBD" company_id: company.id)
+    location_4 = Location.create(city: "Perth", branch: "Leederville" company_id: company.id)
+    location_5 = Location.create(city: "Perth", branch: "Cottesloe" company_id: company.id)
+
+
+    visit company_path(company)
+
+    expect(page).to have_content("Locations:")
+    expect(page).to have_content("Denver - 2")
+    expect(page).to have_content("Perth - 3")
+  end
 end
